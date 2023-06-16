f3c06b4b9d4527876e965b7351ebc0afe6e4993e
MinhTruongHMT <ninjarua2118@gmail.com>
2023-06-16 10:38:29 +0700

    function check for prime number

--- src/controllers/jsoncontroller.js
+++ src/controllers/jsoncontroller.js
@@ -1,16 +1,45 @@
-function hello(req,res){
- res.send("hello");
+function hello(req, res) {
+    res.send("hello");
 }
 
 // The function calculates the sum of two numbers, a and b, obtained as query data
-function sum(req,res){
-    const {a,b} = req.query;
+function sum(req, res) {
+    const { a, b } = req.query;
     res.send({
-        sum:a+b
+        sum: a + b
     })
 }
+//check for prime numbers , obtained as query data
+function checkPrimeNumbers(req, res) {
+    const { number } = req.query;
 
-module.exports={
+    const a =number;//parse number
+
+    const error = "not a prime number";
+    const successful = "a prime number";
+
+    if (a < 2) { //"a number less than 2 is not a prime number
+        return res.send({
+            statusbar: error
+        });
+    }
+    if (a == 2) { //2 is a prime number
+        return res.send({
+            statusbar: successful
+        });
+    }
+    else {
+        for (let i = 2; i <= Math.sqrt(a); i++) { //The category of numbers 1 and itself
+            if (a % i == 0) {//if divisible, it means it is not a prime number
+                return res.send({ statusbar: error });
+            }
+        }
+        res.send({ statusbar: successful });
+    }
+}
+
+module.exports = {
     hello,
-    sum
+    sum,
+    checkPrimeNumbers
 }
--- src/routers/json.js
+++ src/routers/json.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jsonController = require('../controllers/jsoncontroller');
 
 router.get('/hello',jsonController.hello);
-router.get('/sum',jsonController.sum);
+router.get('/number/sum',jsonController.sum);
+router.get('/number/primenumbers',jsonController.checkPrimeNumbers);
 
 module.exports= router;
